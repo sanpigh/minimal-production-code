ef8b4b8bc061b3560b722eb2f7e8eeb599231ef6
Santiago Pighin <pighin.santiago@gmail.com>
2021-09-07 12:42:01 +0200

    add predict branch in simple_API and a workflow that Ill complete with the code coded in taxi_rev

--- app/simple_API.py
+++ app/simple_API.py
@@ -1,4 +1,7 @@
+
+
 from fastapi import FastAPI
+from requests import models
 
 app = FastAPI()
 
@@ -17,3 +20,10 @@ def add_fuction(a, b):
 @app.get('/dif')
 def dif_fuction(a, b):
     return {'a' : a, 'b' : b, 'a-b' : float(a)-float(b)}
+
+
+@app.get('/prediction')
+def prediction-using-model-joblib_fuction(input):
+    model = load_model()
+    fare = model.predict(input)
+    return {'fare' : fare}
