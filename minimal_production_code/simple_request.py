8f883a81d066acca68267ac6ac400a68557cafdd
Santiago Pighin <pighin.santiago@gmail.com>
2021-09-07 10:30:15 +0200

    Nothing to commit, just pushed to trigger google run

--- minimal_production_code/simple_request.py
+++ minimal_production_code/simple_request.py
@@ -1,9 +1,10 @@
 import requests
 
 PORT='8080'
-#url = f'http://127.0.0.1:{PORT}/multiply'
+url = f'http://127.0.0.1:{PORT}/multiply'
 
-url = f'https://minimal-production-code-docker-image-gcp-3jzq5a6kpq-ew.a.run.app/add'
+#url = f'https://minimal-production-code-docker-image-gcp-3jzq5a6kpq-ew.a.run.app/add'
+url = f'https://minimal-production-code-docker-image-gcp-3jzq5a6kpq-ew.a.run.app/multiply'
 
 params = {
     'a' : 13,
