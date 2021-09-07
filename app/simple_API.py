15991e36824304878aac3689f0d9fc1d1b556388
Santiago Pighin <pighin.santiago@gmail.com>
2021-09-07 14:42:57 +0200

    Prediction is not running yet, error concerning instalation of taxi_rev package when attempting to use predict on model.joblib.

--- app/simple_API.py
+++ app/simple_API.py
@@ -1,7 +1,7 @@
-
+import pandas as pd
 import joblib
 from fastapi import FastAPI
-from requests import models
+#from requests import models
 
 app = FastAPI()
 
@@ -23,21 +23,22 @@ def dif_fuction(a, b):
 
 
 @app.get('/prediction')
-def prediction-using-model-joblib_fuction():
+def prediction_fuction():
+    print('inside')
     input = pd.DataFrame({
         'key' : 'nothing', 
         'pickup_datetime'  : '2015-01-27 13:08:24 UTC', 
         'pickup_longitude' : -73.973320, 
         'pickup_latitude'  : 40.835,
-       'dropoff_longitude' : -73.981430, 
-       'dropoff_latitude'  : 40.743835,  
-       'passenger_count'   : 1
-    })
-    model = joblib.load(model.joblib)
-    fare = model.predict(input)
-    print(fare)
-    return True
-#    return {'fare' : fare}
+        'dropoff_longitude' : -73.981430, 
+        'dropoff_latitude'  : 40.743835,  
+        'passenger_count'   : 1
+    }, index=[0])
+    model = joblib.load('models/Lasso/model.joblib')
+    print(type(model))
+#    fare = model.predict(input)
+#    print(fare)
+    return {'fare' : 'fare'}
 
 
 
--- minimal_production_code/simple_request.py
+++ minimal_production_code/simple_request.py
@@ -1,11 +1,12 @@
 import requests
 
-#PORT='8080'
-#url = f'http://127.0.0.1:{PORT}/multiply'
+PORT='8000'
+url = f'http://127.0.0.1:{PORT}/prediction'
 
-url = f'https://minimal-production-code-3jzq5a6kpq-ew.a.run.app/add'
-url = f'https://minimal-production-code-3jzq5a6kpq-ew.a.run.app/dif'
+#url = f'https://minimal-production-code-3jzq5a6kpq-ew.a.run.app/add'
+#url = f'https://minimal-production-code-3jzq5a6kpq-ew.a.run.app/dif'
 #url = f'https://minimal-production-code-3jzq5a6kpq-ew.a.run.app/multiply'
+#url = f'https://minimal-production-code-3jzq5a6kpq-ew.a.run.app/prediction'
 
        
 
@@ -14,7 +15,11 @@ params = {
     'b' : 14
 }
 
-response = requests.get(url, params=params)
-
+#response = requests.get(url, params=params)
+response = requests.get(url)
+print('outside')
 print(response.status_code)
+print('outside2')
+
 print(response.json())
+print('outside3')
