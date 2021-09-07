f3d08a675fb82b047c57447833fbb7de92ac4162
Santiago Pighin <pighin.santiago@gmail.com>
2021-09-07 12:01:08 +0200

    Yet a commit to try to trigger the actualized version of the API

--- minimal_production_code/simple_request.py
+++ minimal_production_code/simple_request.py
@@ -1,7 +1,7 @@
 import requests
 
-PORT='8080'
-url = f'http://127.0.0.1:{PORT}/multiply'
+#PORT='8080'
+#url = f'http://127.0.0.1:{PORT}/multiply'
 
 url = f'https://minimal-production-code-docker-image-gcp-3jzq5a6kpq-ew.a.run.app/add'
 #url = f'https://minimal-production-code-docker-image-gcp-3jzq5a6kpq-ew.a.run.app/multiply'
